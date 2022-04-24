86441d7d59c9253207018f94c9a5924fb4cd3564
Rinat Kurbanov <rkurbanov@cloudlinux.com>
2022-04-24 22:59:41 +0500

    mainpage added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { HeaderComponent } from './UI/header/header.component';
+import { SocialMediaComponent } from './UI/social-media/social-media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    HeaderComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
